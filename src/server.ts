21483d0fd07efc282956ea0e0326bb9b7a214c77
lzhabo <lidia.zhabo99@gmail.com>
2023-04-28 17:29:51 +0300

    :tada:

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { mongoUrl, port } from "./config";
+import { initOrderFetcherCrone } from "./crones/orderFetcherCrone";
+
+// Connect to MongoDB
+// mongoose.Promise = bluebird;
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+  })
+  .catch((err) => {
+    console.log(`❌  MongoDB connection error. Please make sure MongoDB is running. ${err}`);
+    // process.exit();
+  });
+
+initOrderFetcherCrone().then(() => {
+  app.listen(port ?? 5000, () => {
+    console.log("🚀 Server ready at: http://localhost:" + port);
+  });
+});
